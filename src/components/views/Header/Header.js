977bc53e39a6d42df13cd4800134d9d41e8fad96
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-15 23:02:41 +0900

    08/15

--- src/components/views/Header/Header.js
+++ src/components/views/Header/Header.js
@@ -7,6 +7,11 @@ function Header() {
           <div>
               Header
           </div>
+          <input class="burger-check" type="checkbox" id="burger-check" />
+          <label class="burger-icon" for="burger-check"><span class="burger-sticks"></span></label>
+          {/* <div class="menu"> */}
+            {/* <div style="width: 200px"></div> */}
+          {/* </div> */}
         </header>
       </>
   );
