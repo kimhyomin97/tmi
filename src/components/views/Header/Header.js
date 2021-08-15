35307ff071d2537a01eeb5550da32f3a5f66b5ab
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-15 23:55:15 +0900

    08/15

--- src/components/views/Header/Header.js
+++ src/components/views/Header/Header.js
@@ -4,14 +4,24 @@ function Header() {
   return (
       <>
         <header>
-          <div>
-              Header
+          {/* 헤더 선택적으로 나오게 수정 */}
+          <a className="header_logo">TMI</a>
+          <div className="header_item_wrap">
+            <div className="header_item">
+                Header
+            </div>
+            <div className="header_item">
+                menu1
+            </div>
+            <div className="header_item">
+                menu2
+            </div>
           </div>
-          <input class="burger-check" type="checkbox" id="burger-check" />
+          {/* <input class="burger-check" type="checkbox" id="burger-check" />
           <label class="burger-icon" for="burger-check"><span class="burger-sticks"></span></label>
-          {/* <div class="menu"> */}
-            {/* <div style="width: 200px"></div> */}
-          {/* </div> */}
+          <div class="menu">
+            <div style="width: 200px"></div>
+          </div> */}
         </header>
       </>
   );
