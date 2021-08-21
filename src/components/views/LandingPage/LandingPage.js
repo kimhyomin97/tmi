f3befff47255de31aebf910ac49f5b5b58c0b675
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-21 23:46:52 +0900

    08/21

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,26 @@
+import './public/LandingPage.css';
+
 function LandingPage(){
     return(
-        <a href="/homepage">MOVE</a>
+        <>
+        <div className="landingpage_wrap">
+            <a href="/homepage">MOVE</a><br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+            This is Lading Page <br/>
+        </div>
+        </>
     )
 }
 export default LandingPage;
