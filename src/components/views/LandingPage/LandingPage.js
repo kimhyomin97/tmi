7a5b241c84d862166878ba7e6738510f2c10cc9c
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-23 02:44:18 +0900

    08/23

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
 import './public/LandingPage.css';
 
 function LandingPage(){
-    
+    const all = ele => document.querySelectorAll(ele);
+    const one = ele => document.querySelector(ele);
 
+    const slide = () =>{
+        const wrap = one('.slide_wrap');
+        const target = wrap.children[0];
+        const len = target.children.length;
+        target.style.cssText = `width:calc(100% * ${len});display:flex;transition:1s`;
+        
+        Array.from(target.children)
+        .forEach(ele => ele.style.cssText = `width:calc(100% / ${len});`)
+        let pos = 0;
+        setInterval(() => {
+            pos = (pos + 1) % len;
+            target.style.marginLeft = `${-pos * 100}%`;
+        }, 1500)
+    }
+
+    window.onload = function() {slide()};
 
     return(
         <>
