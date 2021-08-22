459111f2905cf7d0d1318a5dc77a52038a17468f
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-22 23:44:42 +0900

    08/22

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -5,6 +5,7 @@ function LandingPage(){
         <>
         <div className="landingpage_wrap">
             <a href="/homepage">MOVE</a><br/>
+            {/* This is Lading Page <br/>
             This is Lading Page <br/>
             This is Lading Page <br/>
             This is Lading Page <br/>
@@ -17,8 +18,13 @@ function LandingPage(){
             This is Lading Page <br/>
             This is Lading Page <br/>
             This is Lading Page <br/>
-            This is Lading Page <br/>
-            This is Lading Page <br/>
+            This is Lading Page <br/> */}
+            <div className="slide_wrap">
+                <div calssName="slide_content">slide1</div>
+                <div calssName="slide_content">slide2</div>
+                <div calssName="slide_content">slide3</div>
+                <div calssName="slide_content">slide4</div>
+            </div>
         </div>
         </>
     )
