f59a1c8c4523025b642669c1dfb8cad44641da94
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-23 19:26:01 +0900

    08/23

---
+++ src/components/function/Slide.js
@@ -0,0 +1,27 @@
+function Slide(){
+    const all = ele => document.querySelectorAll(ele);
+    const one = ele => document.querySelector(ele);
+
+    const slide = () =>{
+        const wrap = one('.slide_wrap');
+        const target = wrap.children[0];
+        const len = target.children.length;
+        target.style.cssText = `width:calc(100% * ${len});display:flex;transition:1s`;
+        
+        Array.from(target.children)
+        .forEach(ele => ele.style.cssText = `width:calc(100% / ${len});`)
+        let pos = 0;
+        setInterval(() => {
+            pos = (pos + 1) % len;
+            target.style.marginLeft = `${-pos * 100}%`;
+        }, 1500)
+    }
+    window.onload = function() {slide()};
+    return(
+        <>
+        <div>slide test</div>
+        </>
+    )
+}
+
+export default Slide;
--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,26 +1,27 @@
 import { useEffect } from 'react';
 import './public/LandingPage.css';
+import Slide from '../../function/Slide';
 
 function LandingPage(){
-    const all = ele => document.querySelectorAll(ele);
-    const one = ele => document.querySelector(ele);
+    // const all = ele => document.querySelectorAll(ele);
+    // const one = ele => document.querySelector(ele);
 
-    const slide = () =>{
-        const wrap = one('.slide_wrap');
-        const target = wrap.children[0];
-        const len = target.children.length;
-        target.style.cssText = `width:calc(100% * ${len});display:flex;transition:1s`;
+    // const slide = () =>{
+    //     const wrap = one('.slide_wrap');
+    //     const target = wrap.children[0];
+    //     const len = target.children.length;
+    //     target.style.cssText = `width:calc(100% * ${len});display:flex;transition:1s`;
         
-        Array.from(target.children)
-        .forEach(ele => ele.style.cssText = `width:calc(100% / ${len});`)
-        let pos = 0;
-        setInterval(() => {
-            pos = (pos + 1) % len;
-            target.style.marginLeft = `${-pos * 100}%`;
-        }, 1500)
-    }
+    //     Array.from(target.children)
+    //     .forEach(ele => ele.style.cssText = `width:calc(100% / ${len});`)
+    //     let pos = 0;
+    //     setInterval(() => {
+    //         pos = (pos + 1) % len;
+    //         target.style.marginLeft = `${-pos * 100}%`;
+    //     }, 1500)
+    // }
 
-    window.onload = function() {slide()};
+    // window.onload = function() {slide()};
 
     return(
         <>
@@ -40,6 +41,7 @@ function LandingPage(){
             This is Lading Page <br/>
             This is Lading Page <br/>
             This is Lading Page <br/> */}
+            <Slide/>
             <div className="slide_wrap">
                 <ul className="slide_box">
                     <li className="slide_content">slide1</li>
