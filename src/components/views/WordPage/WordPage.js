40ce0a9e7505d73deeeb98891b35761016d2d93d
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-25 23:10:13 +0900

    08/25

--- src/components/views/WordPage/WordPage.js
+++ src/components/views/WordPage/WordPage.js
@@ -1,8 +1,23 @@
-function WordPage(){
+import { useState } from "react"
+import './public/WordPage.css';
 
+function WordPage(){
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(!showModal);
+    }
     return(
         <>
         <body>
+            {showModal ?
+                <div>modal_close</div>
+                :
+                <div>modal_open</div>
+            }
+            <div onClick={()=>{openModal()}}>단어 공부하기1</div>
+            <a onClick={()=>{openModal()}}>단어 공부하기2</a><br/>
+            <button className = "word_button" onClick={()=>{openModal()}}>단어 공부하기3</button>
             <div>temp</div><br/>
             <div>temp</div><br/>
             <div>temp</div><br/>
