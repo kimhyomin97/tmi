ca441cc9ff65780e5d3dbbb061288773428e115f
kimhyomin97 <rlagyalsdlek@naver.com>
2021-08-28 20:10:34 +0900

    08/28

--- server/server.js
+++ server/server.js
@@ -19,46 +19,53 @@ var client_secret = process.env.NAVER_TREND_API_CLIENT_SECRET;
 // var client_id = '%REACT_APP_NAVER_TREND_API_CLIENT_ID%';
 // var client_secret = 'REACT_APP_NAVER_TREND_API_CLIENT_SECRET';
 var api_url = 'https://openapi.naver.com/v1/datalab/search';
-var request_body = {
-    "startDate": "2017-01-01",
-    "endDate": "2017-04-30",
-    "timeUnit": "month",
-    "keywordGroups": [
-        {
-            "groupName": "한글",
-            "keywords": [
-                "한글",
-                "korean"
-            ]
-        },
-        {
-            "groupName": "영어",
-            "keywords": [
-                "영어",
-                "english"
-            ]
-        }
-    ],
-    "device": "pc",
-    "ages": [
-        "1",
-        "2"
-    ],
-    "gender": "f"
-};
 
-request.post({
-        url: api_url,
-        body: JSON.stringify(request_body),
-        headers: {
-            'X-Naver-Client-Id': client_id,
-            'X-Naver-Client-Secret': client_secret,
-            'Content-Type': 'application/json'
-        }
-    },
-    function (error, response, body) {
-        console.log(response.statusCode);
-        console.log(body);
-    });
+app.get('/api/trend', (req, res) => {
+    console.log("TEST");
+    // 네이버 검색어 트렌드 api 공부
+    // var request_body = {
+    //     "startDate": "2017-01-01",
+    //     "endDate": "2017-04-30",
+    //     "timeUnit": "month",
+    //     "keywordGroups": [
+    //         {
+    //             "groupName": "한글",
+    //             "keywords": [
+    //                 "한글",
+    //                 "korean"
+    //             ]
+    //         },
+    //         {
+    //             "groupName": "영어",
+    //             "keywords": [
+    //                 "영어",
+    //                 "english"
+    //             ]
+    //         }
+    //     ],
+    //     "device": "pc",
+    //     "ages": [
+    //         "1",
+    //         "2"
+    //     ],
+    //     "gender": "f"
+    // };
+    // request.post({
+    //         url: api_url,
+    //         body: JSON.stringify(request_body),
+    //         headers: {
+    //             'X-Naver-Client-Id': client_id,
+    //             'X-Naver-Client-Secret': client_secret,
+    //             'Content-Type': 'application/json'
+    //         }
+    //     },
+    //     function (error, response, body) {
+    //         // console.log(response.statusCode);
+    //         // console.log(body);
+    //         // console.log(response);
+    //         res.send(response);
+    //     });
+})
+
 
 
--- src/components/views/TrendPage/TrendPage.js
+++ src/components/views/TrendPage/TrendPage.js
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './public/TrendPage.css';
 
 function TrendPage(){
+    const [test, setTest] = useState([]);
+    const [test2, setTest2] = useState([]);
+    const [test3, setTest3] = useState([]);
+    const [test4, setTest4] = useState([]);
+    
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/trend')
+        .then(res => {
+            // console.log(res);
+            // console.log(res.data);
+            // setTest(res.data.statusCode);
+            // setTest2(res.data.body);
+            // setTest3(res.data.headers);
+            // setTest4(res.data.request);
+            
+        })  
+    })
     return(
-        <a href="http://developers.naver.com" target="_blank">
-            <img src="로고 이미지 위치" alt="NAVER 오픈 API" />
-        </a>
+        <>
+        <div>Trend Site</div>
+        {/* <div>{test}</div>
+        <div>{test2}</div>
+        <div>{test3}</div>
+        <div>{test4}</div> */}
+        </>
     )
 }
 
