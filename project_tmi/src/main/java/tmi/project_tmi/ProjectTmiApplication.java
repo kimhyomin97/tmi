49821ba97800bc19baf5ed4fc72d7aa3d5ec4b66
kimhyomin97 <rlagyalsdlek@naver.com>
2021-09-02 23:07:10 +0900

    09/02

--- project_tmi/src/main/java/tmi/project_tmi/ProjectTmiApplication.java
+++ project_tmi/src/main/java/tmi/project_tmi/ProjectTmiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -24,4 +26,10 @@ public class ProjectTmiApplication {
 		String openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU";
 		return "hello this is test";
 	}
+
+//	@GetMapping("/hello-string")
+//	@ResponseBody // HTTP에 head와 body중 body부분을 직접 넣어주겠다
+//	public String helloString(@RequestParam("name") String name){
+//		return "hello " + name; // "hello spring" 문자가 그대로 내려간다
+//	}
 }
