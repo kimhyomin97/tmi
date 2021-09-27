38609e59bcfef969f635ac0786509cf2e59508a3
kimhyomin97 <rlagyalsdlek@naver.com>
2021-09-27 21:52:35 +0900

    ListPage ing

--- src/components/views/ListPage/ListPage.js
+++ src/components/views/ListPage/ListPage.js
@@ -3,8 +3,13 @@ import db from "../../firebase";
 import firebase from 'firebase';
 import { List, ListItem, ListItemText } from "@material-ui/core";
 
+const { kakao } = window;
+
 function ListPage(){
+    const [curlocation, setCurlocation] = useState();
     const [foods, setFoods] = useState([]);
+    const [search, setSearch] = useState(0);
+
     useEffect(() => {
         db.collection('food')
         .orderBy('name', 'desc')
@@ -14,9 +19,45 @@ function ListPage(){
             setFoods(data.docs.map(doc => ({id: doc.id, data: doc.data() })))
         })
     }, [])
+
+    useEffect(() => {
+        var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        var option = { //지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            level: 4 //지도의 레벨(확대, 축소 정도)
+        };
+        option.center = new kakao.maps.LatLng(37.5663795479871, 127.045325760782); // 성동구 마장동으로 위치 변경
+        
+        var geocoder = new kakao.maps.services.Geocoder();
+
+        var callback = function(result, status){
+            if(status === kakao.maps.services.Status.OK){
+                // console.log(result);
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: coords
+                });
+                map.setCenter(coords);
+            }
+        };
+        
+        geocoder.addressSearch(curlocation, callback);
+        var map = new kakao.maps.Map(container, option); //지도 생성 및 객체 리턴
+        // 성동구 마장동
+        // x: "127.045325760782"
+        // y: "37.5663795479871"
+    }, [search])
+
     const [type, setType] = useState("전체");
     const types = ["전체", "한식", "중식", "일식"];
     console.log(foods);
+
+    const inputCurlocation = (e) => {
+        setCurlocation(e.target.value);
+    }
+
     return(
         <>
         <div>test</div>
@@ -34,6 +75,10 @@ function ListPage(){
             )
         })}
         </List>
+        <label>위치 입력 : </label><input type = "text" value = {curlocation} onChange={inputCurlocation} />
+        <button onClick={() => setSearch(search+1)}>입력</button>
+        <div id="map" style={{width:"600px", height:"400px"}}></div>
+
         {
             type=="전체" ? 
             foods?.map(item => {
