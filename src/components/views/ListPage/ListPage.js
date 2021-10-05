ebba392e4a6cb17321a4cd480a5bd3b85c203ec1
kimhyomin97 <rlagyalsdlek@naver.com>
2021-10-05 19:50:57 +0900

    ListPage marker ing

--- src/components/views/ListPage/ListPage.js
+++ src/components/views/ListPage/ListPage.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import db from "../../firebase";
 import firebase from 'firebase';
 import { List, ListItem, ListItemText } from "@material-ui/core";
-import { FontDownloadSharp } from "@material-ui/icons";
+import { FontDownloadSharp, MarkunreadRounded } from "@material-ui/icons";
 
 const { kakao } = window;
 
@@ -10,6 +10,9 @@ function ListPage(){
     const [curlocation, setCurlocation] = useState();
     const [foods, setFoods] = useState([]);
     const [search, setSearch] = useState(0);
+    const [map, setMap] = useState(null);
+    // 이런 방식으로 map을 저장해보는 방법
+    // const [container, setContainer] = useState(null);
 
     useEffect(() => {
         db.collection('food')
@@ -19,27 +22,64 @@ function ListPage(){
             // setFoods(data.docs.map(doc => ({name: doc.name, location: doc.location, price: doc.price, type: doc.type})))
             setFoods(data.docs.map(doc => ({id: doc.id, data: doc.data() })))
         })
-    }, [])
-
-    useEffect(() => {
         var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        // setContainer(document.getElementById('map'));
         var option = { //지도를 생성할 때 필요한 기본 옵션
             center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
             level: 4 //지도의 레벨(확대, 축소 정도)
         };
         option.center = new kakao.maps.LatLng(37.5663795479871, 127.045325760782); // 성동구 마장동으로 위치 변경
-        var map = new kakao.maps.Map(container, option); //지도 생성 및 객체 리턴
-        
+        // var map = new kakao.maps.Map(container, option); //지도 생성 및 객체 리턴
+        setMap(new kakao.maps.Map(container, option));
+    }, [])
+    const setCenter = () => {
+        var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        var option = { //지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            level: 4 //지도의 레벨(확대, 축소 정도)
+        };
         var geocoder = new kakao.maps.services.Geocoder();
-
         var callback = function(result, status){
             if(status === kakao.maps.services.Status.OK){
-                // console.log(result[0].x);
-                option.center = new kakao.maps.LatLng(result[0].y, result[0].x);
-                map = new kakao.maps.Map(container, option);
+                // option.center = new kakao.maps.LatLng(result[0].y, result[0].x);
+                var move_location = new kakao.maps.LatLng(result[0].y, result[0].x);
+                map.setCenter(move_location);
             }
-        };
+        }
         geocoder.addressSearch(curlocation, callback);
+    }
+
+    const setMarker = () => {
+        foods.map(item => {
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(item.data.position.y, item.data.position.x)
+            })
+        // marker.setMap(map);
+        })
+    }
+
+    useEffect(() => {
+        // var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        // var option = { //지도를 생성할 때 필요한 기본 옵션
+        //     center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        //     level: 4 //지도의 레벨(확대, 축소 정도)
+        // };
+        // // option.center = new kakao.maps.LatLng(37.5663795479871, 127.045325760782); // 성동구 마장동으로 위치 변경
+        // // // var map = new kakao.maps.Map(container, option); //지도 생성 및 객체 리턴
+        // // // // setMap(new kakao.maps.Map(container, option));
+        
+        // var geocoder = new kakao.maps.services.Geocoder();
+
+        // var callback = function(result, status){
+        //     if(status === kakao.maps.services.Status.OK){
+        //         // console.log(result[0].x);
+        //         option.center = new kakao.maps.LatLng(result[0].y, result[0].x);
+        //         new kakao.maps.Map(container, option);
+        //         setMap(kakao.maps.Map(container, option));
+        //     }
+        // };
+        // geocoder.addressSearch(curlocation, callback);
 
         // foods.map(item => {
         //     // console.log(item);
@@ -56,15 +96,15 @@ function ListPage(){
         // })
         // // 마커 보이기, 숨기기 버튼 구현하면 된다
 
-        foods.map(item => {
-            // console.log(item.data.position.y, item.data.position.x);
-            // console.log(item.data.position.x);
-            var marker = new kakao.maps.Marker({
-                // map: map,
-                position: new kakao.maps.LatLng(item.data.position.y, item.data.position.x)
-            })
-            marker.setMap(map);
-        })
+        // foods.map(item => {
+        //     // console.log(item.data.position.y, item.data.position.x);
+        //     // console.log(item.data.position.x);
+        //     var marker = new kakao.maps.Marker({
+        //         map: map,
+        //         position: new kakao.maps.LatLng(item.data.position.y, item.data.position.x)
+        //     })
+        //     // marker.setMap(map);
+        // })
 
         // 성동구 마장동
         // x: "127.045325760782"
@@ -123,6 +163,8 @@ function ListPage(){
         </List>
         <label>위치 입력 : </label><input type = "text" value = {curlocation} onChange={inputCurlocation} />
         <button onClick={() => setSearch(search+1)}>입력</button>
+        <button onClick={() => setCenter()}>이동</button>
+        <button onClick={() => setMarker()}>마커표시</button>
         <div id="map" style={{width:"600px", height:"400px"}}></div>
 
         {
