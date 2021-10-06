a07be9f7d7bd16bca40bfbb70ab0146de8c1e7b7
kimhyomin97 <rlagyalsdlek@naver.com>
2021-10-06 23:37:02 +0900

    ListPage style ing

--- src/components/views/ListPage/ListPage.js
+++ src/components/views/ListPage/ListPage.js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 import db from "../../firebase";
 import firebase from 'firebase';
-import { List, ListItem, ListItemText } from "@material-ui/core";
+import { List, ListItem, ListItemText, Divider } from "@material-ui/core";
 import { FontDownloadSharp, MarkunreadRounded } from "@material-ui/icons";
+
+// import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+// import Avatar from '@material-ui/core/Avatar';
+// import ImageIcon from '@material-ui/icons/Image';
+// import WorkIcon from '@material-ui/icons/Work';
+// import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
 const { kakao } = window;
 
@@ -13,6 +23,8 @@ function ListPage(){
     const [map, setMap] = useState(null);
     // 이런 방식으로 map을 저장해보는 방법
     // const [container, setContainer] = useState(null);
+    // const [marker, setMarker] = useState([]);
+    var markers = [];
 
     useEffect(() => {
         db.collection('food')
@@ -25,10 +37,11 @@ function ListPage(){
         var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
         // setContainer(document.getElementById('map'));
         var option = { //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            // center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            center: new kakao.maps.LatLng(36.6116946201537, 127.291002698042),
             level: 4 //지도의 레벨(확대, 축소 정도)
         };
-        option.center = new kakao.maps.LatLng(37.5663795479871, 127.045325760782); // 성동구 마장동으로 위치 변경
+        // option.center = new kakao.maps.LatLng(37.5663795479871, 127.045325760782); // 성동구 마장동으로 위치 변경
         // var map = new kakao.maps.Map(container, option); //지도 생성 및 객체 리턴
         setMap(new kakao.maps.Map(container, option));
     }, [])
@@ -41,6 +54,7 @@ function ListPage(){
         var geocoder = new kakao.maps.services.Geocoder();
         var callback = function(result, status){
             if(status === kakao.maps.services.Status.OK){
+                console.log(result);
                 // option.center = new kakao.maps.LatLng(result[0].y, result[0].x);
                 var move_location = new kakao.maps.LatLng(result[0].y, result[0].x);
                 map.setCenter(move_location);
@@ -144,6 +158,18 @@ function ListPage(){
         setCurlocation(e.target.value);
     }
 
+    const test = () => {
+        return(
+            <div>test</div>
+        )
+    }
+
+    const style = {
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+    };
+
     return(
         <>
         <div>test</div>
@@ -165,16 +191,27 @@ function ListPage(){
         <button onClick={() => setSearch(search+1)}>입력</button>
         <button onClick={() => setCenter()}>이동</button>
         <button onClick={() => setMarker()}>마커표시</button>
+        
         <div id="map" style={{width:"600px", height:"400px"}}></div>
-
+        <List sx={style} component="nav" aria-label="mailbox folders">
         {
             type=="전체" ? 
             foods?.map(item => {
                 return(
                     <>
-                    <a href={`/detail/${item.id}`}>
+                    {/* <a href={`/detail/${item.id}`}>
                         <div>{item.data.name} {item.data.location} {item.data.price} {item.data.type}</div>
-                    </a>
+                    </a> */}
+                    <ListItem button>
+                        {/* <ListItemText
+                            primary={item.data.name}
+                            secondary={item.data.location, item.data.price, item.data.type}
+                         /> */}
+                        <a href={`/detail/${item.id}`}>
+                            <div>{item.data.name} {item.data.location} {item.data.price} {item.data.type}</div>
+                        </a>
+                    </ListItem>
+                    <Divider />
                     </>
                 )
             })
@@ -189,9 +226,11 @@ function ListPage(){
                 }
             })
         }
+        </List>
         계획 <br/>
         1. <br/>
         2. <br/>
+        <button onClick={() => test()}>테스트</button>
         </>
     )
 }
