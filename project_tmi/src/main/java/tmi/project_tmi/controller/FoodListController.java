f760a58accfb497687fa472f2a7c40de342c6fa1
kimhyomin97 <rlagyalsdlek@naver.com>
2022-05-28 15:19:12 +0900

    npm update

--- project_tmi/src/main/java/tmi/project_tmi/controller/FoodListController.java
+++ project_tmi/src/main/java/tmi/project_tmi/controller/FoodListController.java
@@ -1,8 +1,14 @@
-//package tmi.project_tmi.controller;
-//
-//import org.springframework.stereotype.Controller;
-//
-//@Controller
-//public class FoodListController {
-//
-//}
+package tmi.project_tmi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FoodListController {
+    @GetMapping("getfoodlist")
+    public void hello(Model model){
+
+        System.out.println("HELLO");
+    }
+}
