d035aa2959f2e7e7b0b6f7c8f05ce4aacb8e9a5a
kimhyomin97 <rlagyalsdlek@naver.com>
2022-07-04 22:35:22 +0900

    user

--- backend/src/main/java/com/tmi/controller/UserController.java
+++ backend/src/main/java/com/tmi/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.tmi.controller;
 
+
+import com.tmi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+//    @PostMapping("/login")
+//    public User userLogin(@RequestBody User userDto, HttpSession httpSession){
+//
+//    }
 }
--- backend/src/main/java/com/tmi/dto/User.java
+++ backend/src/main/java/com/tmi/dto/User.java
@@ -1,4 +1,14 @@
 package com.tmi.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
+    private String idx;
+    private String userid;
+    private String userpw;
+    private String email;
+    //
 }
