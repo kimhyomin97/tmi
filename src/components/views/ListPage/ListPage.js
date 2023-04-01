29888e724c1abb06282a8d9fc13622bbe2a08cd6
kimhyomin97 <rlagyalsdlek@naver.com>
2023-04-01 23:39:25 +0900

    feat: className 콘솔 오류 제거, 테스트 페이지로 테스트 내용 이동

--- src/components/views/ListPage/ListPage.js
+++ src/components/views/ListPage/ListPage.js
@@ -204,19 +204,6 @@ function ListPage() {
   };
   // const context = useContext(TestContext);
   // console.log(context);
-  const [asyncTest, setAsyncTest] = useState();
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get("https://httpbin.org/delay/5");
-      // 5초에 딜레이가 지난 후 응답을 주는 api
-      // async 테스트를 위한 api
-      setAsyncTest(res.data.url);
-    };
-    fetchData();
-  }, []);
-  const handleTest = () => {
-    console.log(asyncTest);
-  };
 
   return (
     <>
@@ -225,7 +212,6 @@ function ListPage() {
             center={{MA:33.450701, LA:126.570667}} // 지도 중앙위치 설정
             
         ></Map> */}
-      <button onClick={handleTest}>AsyncTest1</button>
       <ButtonGroup
         class="typebt_wrap"
         size="large"
--- src/pages/TestPage/TestPage.js
+++ src/pages/TestPage/TestPage.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setTestMultipleReducer } from "../../store/slice/testMultipleSlice";
 import { setTestReducer } from "../../store/slice/testSlice";
@@ -6,6 +8,18 @@ import ReduxTestPage2 from "../ReduxTestPage/ReduxTestPage2";
 
 const TestPage = () => {
   const dispatch = useDispatch();
+  const [asyncTest, setAsyncTest] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("https://httpbin.org/delay/5");
+      // 5초에 딜레이가 지난 후 응답을 주는 api
+      // async 테스트를 위한 api
+      setAsyncTest(res.data.url);
+    };
+    fetchData();
+  }, []);
+
   const handleTest = () => {
     dispatch(setTestReducer({ testValue: { test1: "test1", test2: "test2" } }));
     dispatch(
@@ -16,9 +30,14 @@ const TestPage = () => {
       })
     );
   };
+
+  const handleAsyncTest = () => {
+    console.log(asyncTest);
+  };
   return (
     <>
       <div>this is TestPage</div>
+      <button onClick={handleAsyncTest}>AsyncTest</button>
       <button onClick={handleTest}>change test value</button>
       <ReduxTestPage1></ReduxTestPage1>
       <ReduxTestPage2></ReduxTestPage2>
